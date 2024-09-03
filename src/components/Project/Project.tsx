6c3f367edc35726921acbad9cbaa9ca7fd4ee8cb
Kaua <kauajuno@gmail.com>
2024-09-03 00:42:02 -0300

    portfolio beta

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,5 +1,66 @@
-const Project = () => {
-  return <div>Project</div>;
+import React from "react";
+import { faGithub, faFigma } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./project.css";
+
+interface ProjectProps {
+  projectImg: string;
+  projectTitle: string;
+  projectDescription: string;
+  figmaLink: string;
+  githubLink: string;
+}
+
+const Project: React.FC<ProjectProps> = ({
+  projectImg,
+  projectTitle,
+  projectDescription,
+  figmaLink,
+  githubLink,
+}) => {
+  return (
+    <div className="project">
+      <div className="project-display">
+        <img src={projectImg} alt="project" className="project-display--img" />
+      </div>
+      <div className="project-info">
+        <h3 className="project-info--title">{projectTitle}</h3>
+        <p className="project-info--description">{projectDescription}</p>
+        <div className="project-info--links">
+          <a
+            href={githubLink}
+            className="project-info--link"
+            onClick={
+              githubLink === "#"
+                ? (e) => {
+                    alert("O link não está pronto ainda!");
+                    e.preventDefault();
+                  }
+                : () => {}
+            }
+          >
+            <FontAwesomeIcon icon={faGithub} />
+            <div>GitHub</div>
+          </a>
+          <a
+            href={figmaLink}
+            className="project-info--link"
+            onClick={
+              figmaLink === "#"
+                ? (e) => {
+                    alert("O link não está pronto ainda!");
+                    e.preventDefault();
+                  }
+                : () => {}
+            }
+          >
+            <FontAwesomeIcon icon={faFigma} />
+            <div>Figma</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,5 +1,36 @@
+import Project from "../../components/Project/Project";
+import Target from "../../assets/target.svg";
+import Notify from "../../assets/notify.svg";
+import MarleyMariaModista from "../../assets/marleymaria.svg";
+import "./projects.css";
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div id="project-section">
+      <h2 id="project-section--title">Projetos</h2>
+      <Project
+        projectImg={Target}
+        projectTitle="Portfólio"
+        projectDescription="Esse portfólio foi criado por mim, desde o design até a codificação."
+        figmaLink="https://www.figma.com/design/IvsK5uSOJ5QDZfvpUKPKkI/Portfolio?node-id=0-1&m=dev&t=s8Y1vIVuSCS2wg8H-1"
+        githubLink="https://github.com/kauajuno/portfolio"
+      />
+      <Project
+        projectImg={Notify}
+        projectTitle="Notify (em breve)"
+        projectDescription="Notify é um aplicativo para tomar notas de maneira simples e confiável."
+        figmaLink="#"
+        githubLink="#"
+      />
+      <Project
+        projectImg={MarleyMariaModista}
+        projectTitle="Marley Maria Modista (em breve)"
+        projectDescription="Marley Maria Modista é um site feita para uma costureira que também é minha mãe."
+        figmaLink="#"
+        githubLink="#"
+      />
+    </div>
+  );
 };
 
 export default Projects;
